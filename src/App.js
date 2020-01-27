941fda515d24273a8022b877e6ddfaf28261ab7e
leesomi <122369@naver.com>
2020-01-27 16:04:31 +0900

    생활코딩 17.3 까지 공부

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
-import React, { Component } from 'react';
-import Subject from './components/Subject';
-import TOC from './components/TOC';
-import Content from './components/Content';
-import './App.css';
+import React, { Component } from "react";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      mode:"read",
-      subject:{title:"WEB", sub:"World Wide Web!"},
-      welcome:{title:"welcome", desc:"Hello, React!"},
-      contents:[
-        {id:1, title:"HTML", desc:"HTML is for information"},
-        {id:2, title:"CSS", desc:"CSS is for design"},
-        {id:3, title:"Javascript", desc:"Javascript is for interactive"}
+    this.state = {
+      mode: "read",
+      selected_content_id: 0,
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "welcome", desc: "Hello, React!" },
+      contents: [
+        { id: 0, title: "HTML", desc: "HTML is for information" },
+        { id: 1, title: "CSS", desc: "CSS is for design" },
+        { id: 2, title: "Javascript", desc: "Javascript is for interactive" }
       ]
-    }
+    };
   }
-  render(){
-    var _title, _desc = null;
-    if(this.state.mode === "welcome"){
+  render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode === "read"){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+    } else if (this.state.mode === "read") {
+      var selected = this.state.selected_content_id;
+      _title = this.state.contents[selected].title;
+      _desc = this.state.contents[selected].desc;
     }
     return (
       <div className="App">
-        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            e.preventDefault();
-            //this.state.mode="welcome";
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function() {
             this.setState({
-              mode:"welcome"
+              mode: "welcome"
             });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
+          }.bind(this)}
+        ></Subject>
+        <TOC
+          data={this.state.contents}
+          onChangePage={function(id) {
+            this.setState({
+              mode: "read",
+              selected_content_id: id
+            });
+          }.bind(this)}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-  class TOC extends Component{
-    constructor(props){
-      super(props);
-      var lists = [];
-      var data = this.props.data;
-      for(var i = 0; i < data.length; i++){
-        lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
-      }
-    }
-    render(){
-      return(
-        <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">Javascript</a></li>
-          </ul>
-        </nav>
+class TOC extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var lists = [];
+    var data = this.props.data;
+    for (var i = 0; i < data.length; i++) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function(e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {data[i].title}
+          </a>
+        </li>
       );
     }
+    return (
+      <nav>
+        <ul>{lists}</ul>
+      </nav>
+    );
   }
+}
 
-  export default TOC;
+export default TOC;
