cc0a7f911b22ce798b3502cdb91a2f91dabd9991
leesomi <122369@naver.com>
2020-02-11 21:11:51 +0900

    생활코딩 react-redux 4 공부

--- src/redux-conponents/AddNumber.js
+++ src/redux-conponents/AddNumber.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-
+import store from "../store";
 export default class AddNumber extends Component {
+  state = { size: 1 };
   render() {
     return (
       <div>
         AddNumber
         <br></br>
-        <input type="button" value="+"></input>
-        <input type="text" value={0}></input>
+        <input
+          type="button"
+          value="+"
+          onClick={function() {
+            console.log("AddNumber onClick");
+            store.dispatch({ type: "INCREMENT", size: this.state.size });
+          }.bind(this)}
+        ></input>
+        <input
+          type="text"
+          value={this.state.size}
+          onChange={function(e) {
+            this.setState({
+              size: Number(e.target.value)
+            });
+          }.bind(this)}
+        ></input>
       </div>
     );
   }
--- src/redux-conponents/DisplayNumber.js
+++ src/redux-conponents/DisplayNumber.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
-
+import store from "../store";
 export default class DisplayNumber extends Component {
+  state = { number: store.getState().number };
+  constructor(props) {
+    super(props);
+    store.subscribe(
+      function() {
+        this.setState({
+          number: store.getState().number
+        });
+      }.bind(this)
+    );
+  }
   render() {
     return (
       <div>
         DisplayNumber
         <br></br>
-        <input type="text" value={0} readonly></input>
+        <input type="text" value={this.state.number} readOnly></input>
       </div>
     );
   }
